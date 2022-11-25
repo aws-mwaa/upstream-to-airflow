de3c3a8dd278eaa728b9863473ef026b371d9bc9
ferruzzi <ferruzzi@amazon.com>
2022-11-25 14:28:31 -0800

    Elasticache

--- tests/providers/amazon/aws/hooks/test_elasticache_replication_group.py
+++ tests/providers/amazon/aws/hooks/test_elasticache_replication_group.py
@@ -17,197 +17,182 @@
 # under the License.
 from __future__ import annotations
 
-from unittest import TestCase
-from unittest.mock import Mock
-
 import pytest
 
 from airflow.exceptions import AirflowException
 from airflow.providers.amazon.aws.hooks.elasticache_replication_group import ElastiCacheReplicationGroupHook
+from tests.providers.amazon.aws.hooks.base_hook_test import BaseHookUnitTest
+
+REPLICATION_GROUP_ID = "test-elasticache-replication-group-hook"
+
+REPLICATION_GROUP_CONFIG = {
+    "ReplicationGroupId": REPLICATION_GROUP_ID,
+    "ReplicationGroupDescription": REPLICATION_GROUP_ID,
+    "AutomaticFailoverEnabled": False,
+    "NumCacheClusters": 1,
+    "CacheNodeType": "cache.m5.large",
+    "Engine": "redis",
+    "EngineVersion": "5.0.4",
+    "CacheParameterGroupName": "default.redis5.0",
+}
+
+VALID_STATES = frozenset(
+    {"creating", "available", "modifying", "deleting", "create - failed", "snapshotting"}
+)
 
 
-class TestElastiCacheReplicationGroupHook(TestCase):
-    REPLICATION_GROUP_ID = "test-elasticache-replication-group-hook"
+@pytest.mark.usefixtures("mock_conn")
+@pytest.mark.parametrize("mock_conn", [ElastiCacheReplicationGroupHook], indirect=True)
+class TestElastiCacheReplicationGroupHook(BaseHookUnitTest):
+    def test_conn_not_none(self):
+        hook = ElastiCacheReplicationGroupHook()
+        assert hook.conn is not None
 
-    REPLICATION_GROUP_CONFIG = {
-        "ReplicationGroupId": REPLICATION_GROUP_ID,
-        "ReplicationGroupDescription": REPLICATION_GROUP_ID,
-        "AutomaticFailoverEnabled": False,
-        "NumCacheClusters": 1,
-        "CacheNodeType": "cache.m5.large",
-        "Engine": "redis",
-        "EngineVersion": "5.0.4",
-        "CacheParameterGroupName": "default.redis5.0",
-    }
-
-    VALID_STATES = frozenset(
-        {"creating", "available", "modifying", "deleting", "create - failed", "snapshotting"}
-    )
-
-    def setUp(self):
-        self.hook = ElastiCacheReplicationGroupHook()
-        # noinspection PyPropertyAccess
-        self.hook.conn = Mock()
-
-        # We need this for every test
-        self.hook.conn.create_replication_group.return_value = {
-            "ReplicationGroup": {"ReplicationGroupId": self.REPLICATION_GROUP_ID, "Status": "creating"}
+    def test_create_replication_group(self, mock_conn):
+        mock_conn().create_replication_group.return_value = {
+            "ReplicationGroup": {"ReplicationGroupId": REPLICATION_GROUP_ID, "Status": "creating"}
         }
 
-    def _create_replication_group(self):
-        return self.hook.create_replication_group(config=self.REPLICATION_GROUP_CONFIG)
+        response = ElastiCacheReplicationGroupHook().create_replication_group(config=REPLICATION_GROUP_CONFIG)
 
-    def test_conn_not_none(self):
-        assert self.hook.conn is not None
-
-    def test_create_replication_group(self):
-        response = self._create_replication_group()
-        assert response["ReplicationGroup"]["ReplicationGroupId"] == self.REPLICATION_GROUP_ID
+        assert response["ReplicationGroup"]["ReplicationGroupId"] == REPLICATION_GROUP_ID
         assert response["ReplicationGroup"]["Status"] == "creating"
 
-    def test_describe_replication_group(self):
-        self._create_replication_group()
-
-        self.hook.conn.describe_replication_groups.return_value = {
-            "ReplicationGroups": [{"ReplicationGroupId": self.REPLICATION_GROUP_ID}]
+    def test_describe_replication_group(self, mock_conn):
+        mock_conn().describe_replication_groups.return_value = {
+            "ReplicationGroups": [{"ReplicationGroupId": REPLICATION_GROUP_ID}]
         }
 
-        response = self.hook.describe_replication_group(replication_group_id=self.REPLICATION_GROUP_ID)
-        assert response["ReplicationGroups"][0]["ReplicationGroupId"] == self.REPLICATION_GROUP_ID
+        response = ElastiCacheReplicationGroupHook().describe_replication_group(
+            replication_group_id=REPLICATION_GROUP_ID
+        )
+        assert response["ReplicationGroups"][0]["ReplicationGroupId"] == REPLICATION_GROUP_ID
 
-    def test_get_replication_group_status(self):
-        self._create_replication_group()
-
-        self.hook.conn.describe_replication_groups.return_value = {
-            "ReplicationGroups": [{"ReplicationGroupId": self.REPLICATION_GROUP_ID, "Status": "available"}]
+    def test_get_replication_group_status(self, mock_conn):
+        mock_conn().describe_replication_groups.return_value = {
+            "ReplicationGroups": [{"ReplicationGroupId": REPLICATION_GROUP_ID, "Status": "available"}]
         }
 
-        response = self.hook.get_replication_group_status(replication_group_id=self.REPLICATION_GROUP_ID)
-        assert response in self.VALID_STATES
+        response = ElastiCacheReplicationGroupHook().get_replication_group_status(
+            replication_group_id=REPLICATION_GROUP_ID
+        )
+        assert response in VALID_STATES
 
-    def test_is_replication_group_available(self):
-        self._create_replication_group()
-
-        self.hook.conn.describe_replication_groups.return_value = {
-            "ReplicationGroups": [{"ReplicationGroupId": self.REPLICATION_GROUP_ID, "Status": "available"}]
+    def test_is_replication_group_available(self, mock_conn):
+        mock_conn().describe_replication_groups.return_value = {
+            "ReplicationGroups": [{"ReplicationGroupId": REPLICATION_GROUP_ID, "Status": "available"}]
         }
 
-        response = self.hook.is_replication_group_available(replication_group_id=self.REPLICATION_GROUP_ID)
+        response = ElastiCacheReplicationGroupHook().is_replication_group_available(
+            replication_group_id=REPLICATION_GROUP_ID
+        )
         assert response in (True, False)
 
-    def test_wait_for_availability(self):
-        self._create_replication_group()
-
+    def test_wait_for_availability(self, mock_conn):
         # Test non availability
-        self.hook.conn.describe_replication_groups.return_value = {
-            "ReplicationGroups": [{"ReplicationGroupId": self.REPLICATION_GROUP_ID, "Status": "creating"}]
+        mock_conn().describe_replication_groups.return_value = {
+            "ReplicationGroups": [{"ReplicationGroupId": REPLICATION_GROUP_ID, "Status": "creating"}]
         }
 
-        response = self.hook.wait_for_availability(
-            replication_group_id=self.REPLICATION_GROUP_ID,
+        response = ElastiCacheReplicationGroupHook().wait_for_availability(
+            replication_group_id=REPLICATION_GROUP_ID,
             max_retries=1,
             initial_sleep_time=1,  # seconds
         )
         assert response is False
 
         # Test availability
-        self.hook.conn.describe_replication_groups.return_value = {
-            "ReplicationGroups": [{"ReplicationGroupId": self.REPLICATION_GROUP_ID, "Status": "available"}]
+        mock_conn().describe_replication_groups.return_value = {
+            "ReplicationGroups": [{"ReplicationGroupId": REPLICATION_GROUP_ID, "Status": "available"}]
         }
 
-        response = self.hook.wait_for_availability(
-            replication_group_id=self.REPLICATION_GROUP_ID,
+        response = ElastiCacheReplicationGroupHook().wait_for_availability(
+            replication_group_id=REPLICATION_GROUP_ID,
             max_retries=1,
             initial_sleep_time=1,  # seconds
         )
         assert response is True
 
-    def test_delete_replication_group(self):
-        self._create_replication_group()
-
-        self.hook.conn.delete_replication_group.return_value = {
-            "ReplicationGroup": {"ReplicationGroupId": self.REPLICATION_GROUP_ID, "Status": "deleting"}
+    def test_delete_replication_group(self, mock_conn):
+        mock_conn().delete_replication_group.return_value = {
+            "ReplicationGroup": {"ReplicationGroupId": REPLICATION_GROUP_ID, "Status": "deleting"}
         }
-
         # Wait for availability, can only delete when replication group is available
-        self.hook.conn.describe_replication_groups.return_value = {
-            "ReplicationGroups": [{"ReplicationGroupId": self.REPLICATION_GROUP_ID, "Status": "available"}]
+        mock_conn().describe_replication_groups.return_value = {
+            "ReplicationGroups": [{"ReplicationGroupId": REPLICATION_GROUP_ID, "Status": "available"}]
         }
 
-        response = self.hook.wait_for_availability(
-            replication_group_id=self.REPLICATION_GROUP_ID,
+        response = ElastiCacheReplicationGroupHook().wait_for_availability(
+            replication_group_id=REPLICATION_GROUP_ID,
             max_retries=1,
             initial_sleep_time=1,  # seconds
         )
         assert response is True
 
-        response = self.hook.delete_replication_group(replication_group_id=self.REPLICATION_GROUP_ID)
-        assert response["ReplicationGroup"]["ReplicationGroupId"] == self.REPLICATION_GROUP_ID
+        response = ElastiCacheReplicationGroupHook().delete_replication_group(
+            replication_group_id=REPLICATION_GROUP_ID
+        )
+        assert response["ReplicationGroup"]["ReplicationGroupId"] == REPLICATION_GROUP_ID
         assert response["ReplicationGroup"]["Status"] == "deleting"
 
-    def _raise_replication_group_not_found_exp(self):
-        self.hook.conn.exceptions.ReplicationGroupNotFoundFault = BaseException
-        return self.hook.conn.exceptions.ReplicationGroupNotFoundFault
+    @staticmethod
+    def _raise_replication_group_not_found_exp(mock_conn):
+        mock_conn().exceptions.ReplicationGroupNotFoundFault = BaseException
+        return mock_conn().exceptions.ReplicationGroupNotFoundFault
 
     # noinspection PyUnusedLocal
-    def _mock_describe_side_effect(self, *args, **kwargs):
+    def _mock_describe_side_effect(self, mock_conn):
         return [
             # On first call replication group is in available state, this will allow to initiate a delete
             # A replication group can only be deleted when it is in `available` state
-            {"ReplicationGroups": [{"ReplicationGroupId": self.REPLICATION_GROUP_ID, "Status": "available"}]},
+            {"ReplicationGroups": [{"ReplicationGroupId": REPLICATION_GROUP_ID, "Status": "available"}]},
             # On second call replication group is in deleting state
-            {"ReplicationGroups": [{"ReplicationGroupId": self.REPLICATION_GROUP_ID, "Status": "deleting"}]},
+            {"ReplicationGroups": [{"ReplicationGroupId": REPLICATION_GROUP_ID, "Status": "deleting"}]},
             # On further calls we will assume the replication group is deleted
-            self._raise_replication_group_not_found_exp(),
+            self._raise_replication_group_not_found_exp(mock_conn),
         ]
 
-    def test_wait_for_deletion(self):
-        self._create_replication_group()
-
-        self.hook.conn.describe_replication_groups.side_effect = self._mock_describe_side_effect()
-
-        self.hook.conn.delete_replication_group.return_value = {
-            "ReplicationGroup": {"ReplicationGroupId": self.REPLICATION_GROUP_ID}
+    def test_wait_for_deletion(self, mock_conn):
+        mock_conn().describe_replication_groups.side_effect = self._mock_describe_side_effect(mock_conn)
+        mock_conn().delete_replication_group.return_value = {
+            "ReplicationGroup": {"ReplicationGroupId": REPLICATION_GROUP_ID}
         }
 
-        response, deleted = self.hook.wait_for_deletion(
-            replication_group_id=self.REPLICATION_GROUP_ID,
+        response, deleted = ElastiCacheReplicationGroupHook().wait_for_deletion(
+            replication_group_id=REPLICATION_GROUP_ID,
             # Initial call - status is `available`
             # 1st retry - status is `deleting`
             # 2nd retry - Replication group is deleted
             max_retries=2,
             initial_sleep_time=1,
         )
-        assert response["ReplicationGroup"]["ReplicationGroupId"] == self.REPLICATION_GROUP_ID
+        assert response["ReplicationGroup"]["ReplicationGroupId"] == REPLICATION_GROUP_ID
         assert deleted is True
 
-    def test_ensure_delete_replication_group_success(self):
-        self._create_replication_group()
-
-        self.hook.conn.describe_replication_groups.side_effect = self._mock_describe_side_effect()
-
-        self.hook.conn.delete_replication_group.return_value = {
-            "ReplicationGroup": {"ReplicationGroupId": self.REPLICATION_GROUP_ID}
+    def test_ensure_delete_replication_group_success(self, mock_conn):
+        mock_conn().describe_replication_groups.side_effect = self._mock_describe_side_effect(mock_conn)
+        mock_conn().delete_replication_group.return_value = {
+            "ReplicationGroup": {"ReplicationGroupId": REPLICATION_GROUP_ID}
         }
 
-        response = self.hook.ensure_delete_replication_group(
-            replication_group_id=self.REPLICATION_GROUP_ID, initial_sleep_time=1, max_retries=2
+        response = ElastiCacheReplicationGroupHook().ensure_delete_replication_group(
+            replication_group_id=REPLICATION_GROUP_ID, initial_sleep_time=1, max_retries=2
         )
 
-        assert response["ReplicationGroup"]["ReplicationGroupId"] == self.REPLICATION_GROUP_ID
+        assert response["ReplicationGroup"]["ReplicationGroupId"] == REPLICATION_GROUP_ID
 
-    def test_ensure_delete_replication_group_failure(self):
-        self._create_replication_group()
-
-        self.hook.conn.describe_replication_groups.side_effect = self._mock_describe_side_effect()
-
-        self.hook.conn.delete_replication_group.return_value = {
-            "ReplicationGroup": {"ReplicationGroupId": self.REPLICATION_GROUP_ID}
+    def test_ensure_delete_replication_group_failure(self, mock_conn):
+        mock_conn().create_replication_group.return_value = {
+            "ReplicationGroup": {"ReplicationGroupId": REPLICATION_GROUP_ID, "Status": "creating"}
+        }
+        mock_conn().describe_replication_groups.side_effect = self._mock_describe_side_effect(mock_conn)
+        mock_conn().delete_replication_group.return_value = {
+            "ReplicationGroup": {"ReplicationGroupId": REPLICATION_GROUP_ID}
         }
 
         with pytest.raises(AirflowException):
             # Try only 1 once with 1 sec buffer time. This will ensure that the `wait_for_deletion` loop
             # breaks quickly before the group is deleted and we get the Airflow exception
-            self.hook.ensure_delete_replication_group(
-                replication_group_id=self.REPLICATION_GROUP_ID, initial_sleep_time=1, max_retries=1
+            ElastiCacheReplicationGroupHook().ensure_delete_replication_group(
+                replication_group_id=REPLICATION_GROUP_ID, initial_sleep_time=1, max_retries=1
             )
