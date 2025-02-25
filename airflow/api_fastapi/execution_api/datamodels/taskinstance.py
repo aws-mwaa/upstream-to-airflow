AshKatzEm/airflow

Correctly support resuming tasks after triggers
There was a number of issues here that prevented us from correctly resuming a
task after it's trigger fired.

First off, only ever ran the execute method and we didn't respect next_method.
So we added that info the the TI context we send in the server in response
to the `.../run` endpoint.

Once we'd fixed that, we then ran into the net problem that we were
incorrectly setting the same value for trigger_kwargs (which are the kwargs we
pass to the trigger constructor) and to the next_kwargs (which are the kwargs
to use when resuming the task) -- they needed to be different. This involved
adding the new field (`kwargs`) onto the TIDeferredStatePayload.

The next complication after that was the "ExtendedJSON" type on the
next_kwargs column of TI: this is a type decorator that automatically applies
the BaseSerialization encode/decode step (__var and __type etc). The problem
with that is that we need to do the serialization on the client in order to
send a JSON HTTP request, so we don't want to encode _again_ on the server
ideally. I was able to do that easily on the write/update side but not so
easily on the read side -- there I left a comment and for now we will hae SQLA
decode it for us, and then we have to encode it again. Not the best, but not a
disaster either.

The other change I did here was to have the DeferTask automatically apply the
serde encoding when serializing, just so that there are fewer places in the
code that need to be aware of that detail (So the Task subprocess will encode
it before making the request toe the Supervisor, and in the Supervisor it will be
kept encoded and passed along as is to it's HTTP request). This means you can
once again pass datetime objects to a trigger "natively", not only strings.

For consistency with the user facing code I renamed `next_method` on
`DeferTask` message to `method_name`. I'm not sure this really makes sense on
in the API request to the API server though.

airflow/api_fastapi/execution_api/datamodels/taskinstance.py
@@ -28,7 +28,6 @@ from pydantic import (
     Tag,
     TypeAdapter,
     WithJsonSchema,
-    field_validator,
 )
 
 from airflow.api_fastapi.common.types import UtcDateTime
@@ -122,15 +121,22 @@ class TIDeferredStatePayload(StrictBaseModel):
         ),
     ]
     classpath: str
-    trigger_kwargs: Annotated[dict[str, Any], Field(default_factory=dict)]
-    next_method: str
-    trigger_timeout: timedelta | None = None
+    trigger_kwargs: Annotated[dict[str, Any] | str, Field(default_factory=dict)]
+    """
+    Kwargs to pass to the trigger constructor, either a plain dict or an ecnrypted string.
 
-    @field_validator("trigger_kwargs")
-    def validate_moment(cls, v):
-        if "moment" in v:
-            v["moment"] = AwareDatetimeAdapter.validate_strings(v["moment"])
-        return v
+    Both forms will be passed along to the trigger, the server will not handle either.
+    """
+
+    trigger_timeout: timedelta | None = None
+    method_name: str
+    """The method on the operator to call when the trigger fires."""
+    kwargs: Annotated[dict[str, Any] | str, Field(default_factory=dict)]
+    """
+    Kwargs to pass to the above method, either a plain dict or an ecnrypted string.
+
+    Both forms will be passed along to the TaskSDK upon resume, the server will not handle either.
+    """
 
 
 class TIRescheduleStatePayload(StrictBaseModel):
@@ -251,6 +257,15 @@ class TIRunContext(BaseModel):
     """Connections that can be accessed by the task instance."""
 
     upstream_map_indexes: dict[str, int] | None = None
+
+    next_method: str | None = None
+    """Method to call. Set when task resumes from a trigger."""
+    next_kwargs: dict[str, Any] | str | None = None
+    """
+    Args to pass to ``next_method``.
+
+    Can either be a "decorated" dict, or a string encrypted with the shared Fernet key.
+    """
 
 
 class PrevSuccessfulDagRunResponse(BaseModel):

airflow/api_fastapi/execution_api/routes/task_instances.py
@@ -17,6 +17,7 @@
 
 from __future__ import annotations
 
+import json
 import logging
 from typing import Annotated
 from uuid import UUID
@@ -78,6 +79,9 @@ def ti_run(
     # We only use UUID above for validation purposes
     ti_id_str = str(task_instance_id)
 
+    from sqlalchemy.sql import column
+    from sqlalchemy.types import JSON
+
     old = (
         select(
             TI.state,
@@ -88,14 +92,28 @@ def ti_run(
             TI.next_method,
             TI.try_number,
             TI.max_tries,
+            TI.next_method,
+            # This selects the raw JSON value, by-passing the deserialization -- we want that to happen on the
+            # client
+            column("next_kwargs", JSON),
         )
+        .select_from(TI)
         .where(TI.id == ti_id_str)
         .with_for_update()
     )
     try:
-        (previous_state, dag_id, run_id, task_id, map_index, next_method, try_number, max_tries) = (
-            session.execute(old).one()
-        )
+        (
+            previous_state,
+            dag_id,
+            run_id,
+            task_id,
+            map_index,
+            next_method,
+            try_number,
+            max_tries,
+            next_method,
+            next_kwargs,
+        ) = session.execute(old).one()
     except NoResultFound:
         log.error("Task Instance %s not found", ti_id_str)
         raise HTTPException(
@@ -195,7 +213,7 @@ def ti_run(
             or 0
         )
 
-        return TIRunContext(
+        context = TIRunContext(
             dag_run=dr,
             task_reschedule_count=task_reschedule_count,
             max_tries=max_tries,
@@ -203,6 +221,13 @@ def ti_run(
             variables=[],
             connections=[],
         )
+
+        # Only set if they are non-null
+        if next_method:
+            context.next_method = next_method
+            context.next_kwargs = next_kwargs
+
+        return context
     except SQLAlchemyError as e:
         log.error("Error marking Task Instance state as running: %s", e)
         raise HTTPException(
@@ -290,10 +315,17 @@ def ti_update_state(
         if ti_patch_payload.trigger_timeout is not None:
             timeout = timezone.utcnow() + ti_patch_payload.trigger_timeout
 
+        trigger_kwargs = ti_patch_payload.trigger_kwargs
+        if not isinstance(trigger_kwargs, str):
+            # If it's passed as a string, assume the client encrypted it, otherwise assume it doesn't need to
+            # be. Just JSON serialize it
+            trigger_kwargs = json.dumps(trigger_kwargs)
+
         trigger_row = Trigger(
             classpath=ti_patch_payload.classpath,
-            kwargs=ti_patch_payload.trigger_kwargs,
+            kwargs={},
         )
+        trigger_row.encrypted_kwargs = trigger_kwargs
         session.add(trigger_row)
         session.flush()
 
@@ -301,11 +333,20 @@ def ti_update_state(
         # either get it from the serialised DAG or get it from the API
 
         query = update(TI).where(TI.id == ti_id_str)
+
+        # This is slightly inefficient as we deserialize it to then right again serialize it in the sqla
+        # TypeAdapter.
+        kwargs = None
+        if ti_patch_payload.kwargs:
+            from airflow.serialization.serialized_objects import BaseSerialization
+
+            kwargs = BaseSerialization.deserialize(ti_patch_payload.kwargs)
+
         query = query.values(
             state=State.DEFERRED,
             trigger_id=trigger_row.id,
-            next_method=ti_patch_payload.next_method,
-            next_kwargs=ti_patch_payload.trigger_kwargs,
+            next_method=ti_patch_payload.method_name,
+            next_kwargs=kwargs,
             trigger_timeout=timeout,
         )
         updated_state = State.DEFERRED

task_sdk/src/airflow/sdk/api/datamodels/_generated.py
@@ -117,9 +117,10 @@ class TIDeferredStatePayload(BaseModel):
     )
     state: Annotated[Literal["deferred"] | None, Field(title="State")] = "deferred"
     classpath: Annotated[str, Field(title="Classpath")]
-    trigger_kwargs: Annotated[dict[str, Any] | None, Field(title="Trigger Kwargs")] = None
-    next_method: Annotated[str, Field(title="Next Method")]
+    trigger_kwargs: Annotated[dict[str, Any] | str | None, Field(title="Trigger Kwargs")] = None
     trigger_timeout: Annotated[timedelta | None, Field(title="Trigger Timeout")] = None
+    method_name: Annotated[str, Field(title="Method Name")]
+    kwargs: Annotated[dict[str, Any] | str | None, Field(title="Kwargs")] = None
 
 
 class TIEnterRunningPayload(BaseModel):
@@ -316,6 +317,8 @@ class TIRunContext(BaseModel):
     variables: Annotated[list[VariableResponse] | None, Field(title="Variables")] = None
     connections: Annotated[list[ConnectionResponse] | None, Field(title="Connections")] = None
     upstream_map_indexes: Annotated[dict[str, int] | None, Field(title="Upstream Map Indexes")] = None
+    next_method: Annotated[str | None, Field(title="Next Method")] = None
+    next_kwargs: Annotated[dict[str, Any] | str | None, Field(title="Next Kwargs")] = None
 
 
 class TITerminalStatePayload(BaseModel):

task_sdk/tests/api/test_client.py
@@ -268,29 +268,34 @@ class TestTaskInstanceOperations:
         # Simulate a successful response from the server that defers a task
         ti_id = uuid6.uuid7()
 
+        msg = DeferTask(
+            classpath="airflow.providers.standard.triggers.temporal.DateTimeTrigger",
+            method_name="execute_complete",
+            trigger_kwargs={
+                "__type": "dict",
+                "__var": {
+                    "moment": {"__type": "datetime", "__var": 1730982899.0},
+                    "end_from_trigger": False,
+                },
+            },
+            kwargs={"__type": "dict", "__var": {}},
+        )
+
         def handle_request(request: httpx.Request) -> httpx.Response:
             if request.url.path == f"/task-instances/{ti_id}/state":
                 actual_body = json.loads(request.read())
                 assert actual_body["state"] == "deferred"
-                assert actual_body["trigger_kwargs"] == {
-                    "moment": "2024-11-07T12:34:59Z",
-                    "end_from_trigger": False,
-                }
+                assert actual_body["trigger_kwargs"] == msg.trigger_kwargs
                 assert (
                     actual_body["classpath"] == "airflow.providers.standard.triggers.temporal.DateTimeTrigger"
                 )
-                assert actual_body["next_method"] == "execute_complete"
+                assert actual_body["method_name"] == "execute_complete"
                 return httpx.Response(
                     status_code=204,
                 )
             return httpx.Response(status_code=400, json={"detail": "Bad Request"})
 
         client = make_client(transport=httpx.MockTransport(handle_request))
-        msg = DeferTask(
-            classpath="airflow.providers.standard.triggers.temporal.DateTimeTrigger",
-            trigger_kwargs={"moment": "2024-11-07T12:34:59Z", "end_from_trigger": False},
-            next_method="execute_complete",
-        )
         client.task_instances.defer(ti_id, msg)
 
     def test_task_instance_reschedule(self):

task_sdk/tests/execution_time/test_task_runner.py
@@ -24,6 +24,7 @@ import uuid
 from datetime import datetime, timedelta
 from pathlib import Path
 from socket import socketpair
+from typing import TYPE_CHECKING
 from unittest import mock
 from unittest.mock import patch
 
@@ -85,6 +86,9 @@ from airflow.utils import timezone
 from airflow.utils.state import TaskInstanceState
 
 from tests_common.test_utils.mock_operators import AirflowLink
+
+if TYPE_CHECKING:
+    from kgb import SpyAgency
 
 FAKE_BUNDLE = BundleInfo(name="anything", version="any")
 
@@ -183,15 +187,13 @@ def test_parse(test_dags_dir: Path, make_ti_context):
 
 def test_run_deferred_basic(time_machine, create_runtime_ti, mock_supervisor_comms):
     """Test that a task can transition to a deferred state."""
-    import datetime
-
     from airflow.providers.standard.sensors.date_time import DateTimeSensorAsync
 
     # Use the time machine to set the current time
     instant = timezone.datetime(2024, 11, 22)
     task = DateTimeSensorAsync(
         task_id="async",
-        target_time=str(instant + datetime.timedelta(seconds=3)),
+        target_time=str(instant + timedelta(seconds=3)),
         poke_interval=60,
         timeout=600,
     )
@@ -201,20 +203,49 @@ def test_run_deferred_basic(time_machine, create_runtime_ti, mock_supervisor_com
     expected_defer_task = DeferTask(
         state="deferred",
         classpath="airflow.providers.standard.triggers.temporal.DateTimeTrigger",
+        # Since we are in the task process here, we expect this to have not been encoded by serde yet
         trigger_kwargs={
             "end_from_trigger": False,
             "moment": instant + timedelta(seconds=3),
         },
-        next_method="execute_complete",
         trigger_timeout=None,
+        method_name="execute_complete",
+        kwargs={},
     )
 
     # Run the task
     ti = create_runtime_ti(dag_id="basic_deferred_run", task=task)
-    run(ti, log=mock.MagicMock())
+    state, msg, err = run(ti, log=mock.MagicMock())
 
     # send_request will only be called when the TaskDeferred exception is raised
     mock_supervisor_comms.send_request.assert_any_call(msg=expected_defer_task, log=mock.ANY)
+
+
+def test_resume_from_deferred(time_machine, create_runtime_ti, mock_supervisor_comms, spy_agency: SpyAgency):
+    from airflow.providers.standard.sensors.date_time import DateTimeSensorAsync
+
+    instant_str = "2024-09-30T12:00:00Z"
+    instant = timezone.parse(instant_str)
+    task = DateTimeSensorAsync(
+        task_id="async",
+        target_time=instant + timedelta(seconds=3),
+        poke_interval=60,
+        timeout=600,
+    )
+
+    ti = create_runtime_ti(dag_id="basic_deferred_run", task=task)
+    ti._ti_context_from_server.next_method = "execute_complete"
+    ti._ti_context_from_server.next_kwargs = {
+        "__type": "dict",
+        "__var": {"event": {"__type": "datetime", "__var": 1727697600.0}},
+    }
+
+    spy = spy_agency.spy_on(task.execute_complete)
+    state, msg, err = run(ti, log=mock.MagicMock())
+    assert err is None
+    assert state == TaskInstanceState.SUCCESS
+
+    spy_agency.assert_spy_called_with(spy, mock.ANY, event=instant)
 
 
 def test_run_basic_skipped(time_machine, create_runtime_ti, mock_supervisor_comms):

aws-mwaa/upstream-to-airflow

airflow/api_fastapi/execution_api/datamodels/taskinstance.py
@@ -28,7 +28,6 @@ from pydantic import (
     Tag,
     TypeAdapter,
     WithJsonSchema,
-    field_validator,
 )
 
 from airflow.api_fastapi.common.types import UtcDateTime
@@ -122,15 +121,22 @@ class TIDeferredStatePayload(StrictBaseModel):
         ),
     ]
     classpath: str
-    trigger_kwargs: Annotated[dict[str, Any], Field(default_factory=dict)]
-    next_method: str
-    trigger_timeout: timedelta | None = None
+    trigger_kwargs: Annotated[dict[str, Any] | str, Field(default_factory=dict)]
+    """
+    Kwargs to pass to the trigger constructor, either a plain dict or an ecnrypted string.
 
-    @field_validator("trigger_kwargs")
-    def validate_moment(cls, v):
-        if "moment" in v:
-            v["moment"] = AwareDatetimeAdapter.validate_strings(v["moment"])
-        return v
+    Both forms will be passed along to the trigger, the server will not handle either.
+    """
+
+    trigger_timeout: timedelta | None = None
+    next_method: str
+    """The name of themethod on the operator to call in the worker after the trigger has fired."""
+    next_kwargs: Annotated[dict[str, Any] | str, Field(default_factory=dict)]
+    """
+    Kwargs to pass to the above method, either a plain dict or an ecnrypted string.
+
+    Both forms will be passed along to the TaskSDK upon resume, the server will not handle either.
+    """
 
 
 class TIRescheduleStatePayload(StrictBaseModel):
@@ -251,6 +257,15 @@ class TIRunContext(BaseModel):
     """Connections that can be accessed by the task instance."""
 
     upstream_map_indexes: dict[str, int] | None = None
+
+    next_method: str | None = None
+    """Method to call. Set when task resumes from a trigger."""
+    next_kwargs: dict[str, Any] | str | None = None
+    """
+    Args to pass to ``next_method``.
+
+    Can either be a "decorated" dict, or a string encrypted with the shared Fernet key.
+    """
 
 
 class PrevSuccessfulDagRunResponse(BaseModel):

airflow/api_fastapi/execution_api/routes/task_instances.py
@@ -17,6 +17,7 @@
 
 from __future__ import annotations
 
+import json
 import logging
 from typing import Annotated
 from uuid import UUID
@@ -78,6 +79,9 @@ def ti_run(
     # We only use UUID above for validation purposes
     ti_id_str = str(task_instance_id)
 
+    from sqlalchemy.sql import column
+    from sqlalchemy.types import JSON
+
     old = (
         select(
             TI.state,
@@ -88,14 +92,28 @@ def ti_run(
             TI.next_method,
             TI.try_number,
             TI.max_tries,
+            TI.next_method,
+            # This selects the raw JSON value, by-passing the deserialization -- we want that to happen on the
+            # client
+            column("next_kwargs", JSON),
         )
+        .select_from(TI)
         .where(TI.id == ti_id_str)
         .with_for_update()
     )
     try:
-        (previous_state, dag_id, run_id, task_id, map_index, next_method, try_number, max_tries) = (
-            session.execute(old).one()
-        )
+        (
+            previous_state,
+            dag_id,
+            run_id,
+            task_id,
+            map_index,
+            next_method,
+            try_number,
+            max_tries,
+            next_method,
+            next_kwargs,
+        ) = session.execute(old).one()
     except NoResultFound:
         log.error("Task Instance %s not found", ti_id_str)
         raise HTTPException(
@@ -195,7 +213,7 @@ def ti_run(
             or 0
         )
 
-        return TIRunContext(
+        context = TIRunContext(
             dag_run=dr,
             task_reschedule_count=task_reschedule_count,
             max_tries=max_tries,
@@ -203,6 +221,13 @@ def ti_run(
             variables=[],
             connections=[],
         )
+
+        # Only set if they are non-null
+        if next_method:
+            context.next_method = next_method
+            context.next_kwargs = next_kwargs
+
+        return context
     except SQLAlchemyError as e:
         log.error("Error marking Task Instance state as running: %s", e)
         raise HTTPException(
@@ -290,10 +315,17 @@ def ti_update_state(
         if ti_patch_payload.trigger_timeout is not None:
             timeout = timezone.utcnow() + ti_patch_payload.trigger_timeout
 
+        trigger_kwargs = ti_patch_payload.trigger_kwargs
+        if not isinstance(trigger_kwargs, str):
+            # If it's passed as a string, assume the client encrypted it, otherwise assume it doesn't need to
+            # be. Just JSON serialize it
+            trigger_kwargs = json.dumps(trigger_kwargs)
+
         trigger_row = Trigger(
             classpath=ti_patch_payload.classpath,
-            kwargs=ti_patch_payload.trigger_kwargs,
+            kwargs={},
         )
+        trigger_row.encrypted_kwargs = trigger_kwargs
         session.add(trigger_row)
         session.flush()
 
@@ -301,11 +333,20 @@ def ti_update_state(
         # either get it from the serialised DAG or get it from the API
 
         query = update(TI).where(TI.id == ti_id_str)
+
+        # This is slightly inefficient as we deserialize it to then right again serialize it in the sqla
+        # TypeAdapter.
+        next_kwargs = None
+        if ti_patch_payload.next_kwargs:
+            from airflow.serialization.serialized_objects import BaseSerialization
+
+            next_kwargs = BaseSerialization.deserialize(ti_patch_payload.next_kwargs)
+
         query = query.values(
             state=State.DEFERRED,
             trigger_id=trigger_row.id,
             next_method=ti_patch_payload.next_method,
-            next_kwargs=ti_patch_payload.trigger_kwargs,
+            next_kwargs=next_kwargs,
             trigger_timeout=timeout,
         )
         updated_state = State.DEFERRED

task_sdk/src/airflow/sdk/api/datamodels/_generated.py
@@ -117,9 +117,10 @@ class TIDeferredStatePayload(BaseModel):
     )
     state: Annotated[Literal["deferred"] | None, Field(title="State")] = "deferred"
     classpath: Annotated[str, Field(title="Classpath")]
-    trigger_kwargs: Annotated[dict[str, Any] | None, Field(title="Trigger Kwargs")] = None
-    next_method: Annotated[str, Field(title="Next Method")]
+    trigger_kwargs: Annotated[dict[str, Any] | str | None, Field(title="Trigger Kwargs")] = None
     trigger_timeout: Annotated[timedelta | None, Field(title="Trigger Timeout")] = None
+    next_method: Annotated[str, Field(title="Next Method")]
+    next_kwargs: Annotated[dict[str, Any] | str | None, Field(title="Next Kwargs")] = None
 
 
 class TIEnterRunningPayload(BaseModel):
@@ -316,6 +317,8 @@ class TIRunContext(BaseModel):
     variables: Annotated[list[VariableResponse] | None, Field(title="Variables")] = None
     connections: Annotated[list[ConnectionResponse] | None, Field(title="Connections")] = None
     upstream_map_indexes: Annotated[dict[str, int] | None, Field(title="Upstream Map Indexes")] = None
+    next_method: Annotated[str | None, Field(title="Next Method")] = None
+    next_kwargs: Annotated[dict[str, Any] | str | None, Field(title="Next Kwargs")] = None
 
 
 class TITerminalStatePayload(BaseModel):

task_sdk/tests/api/test_client.py
@@ -268,14 +268,24 @@ class TestTaskInstanceOperations:
         # Simulate a successful response from the server that defers a task
         ti_id = uuid6.uuid7()
 
+        msg = DeferTask(
+            classpath="airflow.providers.standard.triggers.temporal.DateTimeTrigger",
+            next_method="execute_complete",
+            trigger_kwargs={
+                "__type": "dict",
+                "__var": {
+                    "moment": {"__type": "datetime", "__var": 1730982899.0},
+                    "end_from_trigger": False,
+                },
+            },
+            next_kwargs={"__type": "dict", "__var": {}},
+        )
+
         def handle_request(request: httpx.Request) -> httpx.Response:
             if request.url.path == f"/task-instances/{ti_id}/state":
                 actual_body = json.loads(request.read())
                 assert actual_body["state"] == "deferred"
-                assert actual_body["trigger_kwargs"] == {
-                    "moment": "2024-11-07T12:34:59Z",
-                    "end_from_trigger": False,
-                }
+                assert actual_body["trigger_kwargs"] == msg.trigger_kwargs
                 assert (
                     actual_body["classpath"] == "airflow.providers.standard.triggers.temporal.DateTimeTrigger"
                 )
@@ -286,11 +296,6 @@ class TestTaskInstanceOperations:
             return httpx.Response(status_code=400, json={"detail": "Bad Request"})
 
         client = make_client(transport=httpx.MockTransport(handle_request))
-        msg = DeferTask(
-            classpath="airflow.providers.standard.triggers.temporal.DateTimeTrigger",
-            trigger_kwargs={"moment": "2024-11-07T12:34:59Z", "end_from_trigger": False},
-            next_method="execute_complete",
-        )
         client.task_instances.defer(ti_id, msg)
 
     def test_task_instance_reschedule(self):

task_sdk/tests/execution_time/test_task_runner.py
@@ -24,6 +24,7 @@ import uuid
 from datetime import datetime, timedelta
 from pathlib import Path
 from socket import socketpair
+from typing import TYPE_CHECKING
 from unittest import mock
 from unittest.mock import patch
 
@@ -85,6 +86,9 @@ from airflow.utils import timezone
 from airflow.utils.state import TaskInstanceState
 
 from tests_common.test_utils.mock_operators import AirflowLink
+
+if TYPE_CHECKING:
+    from kgb import SpyAgency
 
 FAKE_BUNDLE = BundleInfo(name="anything", version="any")
 
@@ -183,15 +187,13 @@ def test_parse(test_dags_dir: Path, make_ti_context):
 
 def test_run_deferred_basic(time_machine, create_runtime_ti, mock_supervisor_comms):
     """Test that a task can transition to a deferred state."""
-    import datetime
-
     from airflow.providers.standard.sensors.date_time import DateTimeSensorAsync
 
     # Use the time machine to set the current time
     instant = timezone.datetime(2024, 11, 22)
     task = DateTimeSensorAsync(
         task_id="async",
-        target_time=str(instant + datetime.timedelta(seconds=3)),
+        target_time=str(instant + timedelta(seconds=3)),
         poke_interval=60,
         timeout=600,
     )
@@ -201,20 +203,49 @@ def test_run_deferred_basic(time_machine, create_runtime_ti, mock_supervisor_com
     expected_defer_task = DeferTask(
         state="deferred",
         classpath="airflow.providers.standard.triggers.temporal.DateTimeTrigger",
+        # Since we are in the task process here, we expect this to have not been encoded by serde yet
         trigger_kwargs={
             "end_from_trigger": False,
             "moment": instant + timedelta(seconds=3),
         },
-        next_method="execute_complete",
         trigger_timeout=None,
+        next_method="execute_complete",
+        next_kwargs={},
     )
 
     # Run the task
     ti = create_runtime_ti(dag_id="basic_deferred_run", task=task)
-    run(ti, log=mock.MagicMock())
+    state, msg, err = run(ti, log=mock.MagicMock())
 
     # send_request will only be called when the TaskDeferred exception is raised
     mock_supervisor_comms.send_request.assert_any_call(msg=expected_defer_task, log=mock.ANY)
+
+
+def test_resume_from_deferred(time_machine, create_runtime_ti, mock_supervisor_comms, spy_agency: SpyAgency):
+    from airflow.providers.standard.sensors.date_time import DateTimeSensorAsync
+
+    instant_str = "2024-09-30T12:00:00Z"
+    instant = timezone.parse(instant_str)
+    task = DateTimeSensorAsync(
+        task_id="async",
+        target_time=instant + timedelta(seconds=3),
+        poke_interval=60,
+        timeout=600,
+    )
+
+    ti = create_runtime_ti(dag_id="basic_deferred_run", task=task)
+    ti._ti_context_from_server.next_method = "execute_complete"
+    ti._ti_context_from_server.next_kwargs = {
+        "__type": "dict",
+        "__var": {"event": {"__type": "datetime", "__var": 1727697600.0}},
+    }
+
+    spy = spy_agency.spy_on(task.execute_complete)
+    state, msg, err = run(ti, log=mock.MagicMock())
+    assert err is None
+    assert state == TaskInstanceState.SUCCESS
+
+    spy_agency.assert_spy_called_with(spy, mock.ANY, event=instant)
 
 
 def test_run_basic_skipped(time_machine, create_runtime_ti, mock_supervisor_comms):