953b110fa0e0bbc7c75335f22e25d7bcce58fd67
Niko Oliveira <onikolas@amazon.com>
2025-05-13 11:09:47 -0700

    Fix docs build

--- providers/amazon/tests/system/amazon/aws/utils/__init__.py
+++ providers/amazon/tests/system/amazon/aws/utils/__init__.py
@@ -112,9 +112,11 @@ def _fetch_from_ssm(key: str, test_name: str | None = None) -> str:
     except NoCredentialsError as e:
         log.info("No boto credentials found: %s", e)
     except ClientError as e:
-        log.info("Client error when connecting to SSM: %s", e)
-    except hook.conn.exceptions.ParameterNotFound as e:
-        log.info("SSM does not contain any parameter for this test: %s", e)
+        error_code = e.response.get("Error", {}).get("Code", "")
+        if error_code == "ParameterNotFound":
+            log.info("SSM does not contain any parameter for this test: %s", e)
+        else:
+            log.info("Client error when connecting to SSM: %s", e)
     except KeyError as e:
         log.info(
             "SSM contains one parameter for this test, but not the requested value: %s",
